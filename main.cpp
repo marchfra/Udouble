7ae30123f78025adbd9712b6e66682e4e89b33c7
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2021-11-19 16:46:19 +0100

    Implemented Udouble class. Added basic operations between Udoubles and correct formatting of values with uncertainty.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "Udouble.h"
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	Udouble x(10, 0.1);
+	Udouble y(0, 0.1);
+	std::cout << "x = " << x;
+	std::cout << "y = " << y;
+	std::cout << "x + y = " << x + y;
+	std::cout << "x - y = " << x - y;
+	std::cout << "x * y = " << x * y;
+	std::cout << "x / y = " << x / y;
+
 	return 0;
 }
